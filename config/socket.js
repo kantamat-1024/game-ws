d6526439bb167da52385f98ac6b1add380c446fc
kantamat <kantamat@amazon.co.jp>
2023-12-26 13:24:13 +0900

    socket.js

--- config/socket.js
+++ config/socket.js
@@ -3,189 +3,189 @@ const io = require('socket.io');
 
 module.exports = function(server) {
 
-  // サーバーを使ってSocket.IOサーバーを作成
-  const ioServer = io(server);
+ // サーバーを使ってSocket.IOサーバーを作成
+ const ioServer = io(server);
 
-  // chess.jsをインポート
-  const chess = require('chess.js');
+ // chess.jsをインポート
+ const chess = require('chess.js');
 
-  // トップレーティングゲームのチェス盤を作成
-  const topRatedGame = new chess.Chess();
+ // トップレーティングゲームのチェス盤を作成
+ const topRatedGame = new chess.Chess();
 
-  // トップレーティングゲーム用の名前空間
-  const tvNamespace = ioServer.of('/tv');
+ // トップレーティングゲーム用の名前空間
+ const tvNamespace = ioServer.of('/tv');
 
-  // トップレーティングゲームの状態を定期的に送信
-  setInterval(() => {
+ // トップレーティングゲームの状態を定期的に送信
+ setInterval(() => {
 
-    var possibleMoves = topRatedGame.moves();
+   var possibleMoves = topRatedGame.moves();
 
-    if (topRatedGame.game_over() || topRatedGame.in_draw() || possibleMoves.length === 0) {
-      topRatedGame = new chess.Chess();
-      possibleMoves = topRatedGame.moves();
-    }
+   if (topRatedGame.game_over() || topRatedGame.in_draw() || possibleMoves.length === 0) {
+     topRatedGame = new chess.Chess();
+     possibleMoves = topRatedGame.moves();
+   }
 
-    const move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
-    topRatedGame.move(move);
+   const move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+   topRatedGame.move(move);
 
-    tvNamespace.emit('new-top-rated-game-move', {
-      fen: topRatedGame.fen(),
-      pgn: topRatedGame.pgn(),
-      turn: topRatedGame.turn()
-    });
+   tvNamespace.emit('new-top-rated-game-move', {
+     fen: topRatedGame.fen(),
+     pgn: topRatedGame.pgn(),
+     turn: topRatedGame.turn()
+   });
 
-  }, 3000);
+ }, 3000);
 
-  // トップレーティングゲームの名前空間の接続時に状態を送信
-  tvNamespace.on('connection', socket => {
-    socket.emit('new-top-rated-game-move', {
-      fen: topRatedGame.fen(),
-      pgn: topRatedGame.pgn(),
-      turn: topRatedGame.turn()
-    });
-  });
+ // トップレーティングゲームの名前空間の接続時に状態を送信
+ tvNamespace.on('connection', socket => {
+   socket.emit('new-top-rated-game-move', {
+     fen: topRatedGame.fen(),
+     pgn: topRatedGame.pgn(),
+     turn: topRatedGame.turn()
+   });
+ });
 
-  // ゲームとユーザー数を保持する変数
-  const games = {};
-  let users = 0;
+ // ゲームとユーザー数を保持する変数
+ const games = {};
+ let users = 0;
 
-  // モニタリング用の名前空間
-  const monitorNamespace = ioServer.of('/monitor');
+ // モニタリング用の名前空間
+ const monitorNamespace = ioServer.of('/monitor');
 
-  // モニタリング名前空間の接続時に現在の状態を送信
-  monitorNamespace.on('connection', socket => {
-    socket.emit('update', {
-      nbUsers: users,
-      nbGames: Object.keys(games).length
-    });
-  });
+ // モニタリング名前空間の接続時に現在の状態を送信
+ monitorNamespace.on('connection', socket => {
+   socket.emit('update', {
+     nbUsers: users,
+     nbGames: Object.keys(games).length
+   });
+ });
 
-  // メインのSocket.IOサーバーへの接続時
-  ioServer.on('connection', socket => {
+ // メインのSocket.IOサーバーへの接続時
+ ioServer.on('connection', socket => {
 
-    // ユーザー名を取得
-    const username = socket.handshake.query.user;
+   // ユーザー名を取得
+   const username = socket.handshake.query.user;
 
-    // ユーザー数を更新
-    users++;
+   // ユーザー数を更新
+   users++;
 
-    // モニタリング名前空間に現在の状態を送信
-    monitorNamespace.emit('update', {
-      nbUsers: users,
-      nbGames: Object.keys(games).length
-    });
+   // モニタリング名前空間に現在の状態を送信
+   monitorNamespace.emit('update', {
+     nbUsers: users,
+     nbGames: Object.keys(games).length
+   });
 
-    // ゲーム参加時
-    socket.on('join', function(data) {
+   // ゲーム参加時
+   socket.on('join', function(data) {
 
-      // ゲームルーム名
-      const room = data.token;
+     // ゲームルーム名
+     const room = data.token;
 
-      // ゲームがまだない場合は初期化
-      if (!games[room]) {
-        
-        var players = [
-          {
-            socket: socket,
-            name: username,
-            status: 'joined',
-            side: data.side
-          },
-          {
-            socket: null,
-            name: "",
-            status: 'open',
-            side: data.side === "black" ? "white" : "black"
-          }
-        ];
+     // ゲームがまだない場合は初期化
+     if (!games[room]) {
+       
+       var players = [
+         {
+           socket: socket,
+           name: username,
+           status: 'joined',
+           side: data.side
+         },
+         {
+           socket: null,
+           name: "",
+           status: 'open',
+           side: data.side === "black" ? "white" : "black"
+         }
+       ];
 
-        games[room] = {
-          room: room,
-          creator: socket,
-          status: 'waiting',
-          creationDate: Date.now(),
-          players: players
-        };
+       games[room] = {
+         room: room,
+         creator: socket,
+         status: 'waiting',
+         creationDate: Date.now(),
+         players: players
+       };
 
-        socket.join(room);
-        socket.emit('wait');
-        return;
-      }
+       socket.join(room);
+       socket.emit('wait');
+       return;
+     }
 
-      // 2人目のプレイヤーが参加
-      var game = games[room];
-      socket.join(room);
-      game.players[1].socket = socket;
-      game.players[1].name = username;
-      game.players[1].status = "joined";
-      game.status = "ready";
-      io.sockets.to(room).emit('ready', {
-        white: getPlayerName(room, "white"),
-        black: getPlayerName(room, "black")
-      });
+     // 2人目のプレイヤーが参加
+     var game = games[room];
+     socket.join(room);
+     game.players[1].socket = socket;
+     game.players[1].name = username;
+     game.players[1].status = "joined";
+     game.status = "ready";
+     io.sockets.to(room).emit('ready', {
+       white: getPlayerName(room, "white"),
+       black: getPlayerName(room, "black")
+     });
 
-    });
+   });
 
-    // 新しい手の通知
-    socket.on('new-move', function(data) {
-      socket.broadcast.to(data.token).emit('new-move', data);
-    });
+   // 新しい手の通知
+   socket.on('new-move', function(data) {
+     socket.broadcast.to(data.token).emit('new-move', data);
+   });
 
-    // 降参時
-    socket.on('resign', function(data) {
-      const room = data.token;
-      if (room in games) {
-        io.sockets.to(room).emit('player-resigned', {
-          side: data.side
-        });
-        delete games[room];
-        
-        games[room].players[0].socket.leave(room);
-        games[room].players[1].socket.leave(room);
+   // 降参時
+   socket.on('resign', function(data) {
+     const room = data.token;
+     if (room in games) {
+       io.sockets.to(room).emit('player-resigned', {
+         side: data.side
+       });
+       delete games[room];
+       
+       games[room].players[0].socket.leave(room);
+       games[room].players[1].socket.leave(room);
 
-        monitorNamespace.emit('update', {
-          nbUsers: users,
-          nbGames: Object.keys(games).length
-        });
-      }
-    });
+       monitorNamespace.emit('update', {
+         nbUsers: users,
+         nbGames: Object.keys(games).length
+       });
+     }
+   });
 
-    // 切断時
-    socket.on('disconnect', function(data) {
-      users--;
-      monitorNamespace.emit('update', {
-        nbUsers: users, 
-        nbGames: Object.keys(games).length
-      });
-      
-      for (var token in games) {
-        var game = games[token];
-        for (var p in game.players) {
-          var player = game.players[p];
-          if (player.socket === socket) {
-            socket.broadcast.to(token).emit('opponent-disconnected');
-            delete games[token];
-            
-            monitorNamespace.emit('update', {
-              nbUsers: users,
-              nbGames: Object.keys(games).length
-            });
-          }
-        }
-      }
-    });
+   // 切断時
+   socket.on('disconnect', function(data) {
+     users--;
+     monitorNamespace.emit('update', {
+       nbUsers: users, 
+       nbGames: Object.keys(games).length
+     });
+     
+     for (var token in games) {
+       var game = games[token];
+       for (var p in game.players) {
+         var player = game.players[p];
+         if (player.socket === socket) {
+           socket.broadcast.to(token).emit('opponent-disconnected');
+           delete games[token];
+           
+           monitorNamespace.emit('update', {
+             nbUsers: users,
+             nbGames: Object.keys(games).length
+           });
+         }
+       }
+     }
+   });
 
-  });
+ });
 
-  // プレイヤー名を取得
-  function getPlayerName(room, side) {
-    var game = games[room];
-    for (var p in game.players) {
-      var player = game.players[p];
-      if (player.side === side) {
-        return player.name;
-      }
-    }
-  }
+ // プレイヤー名を取得
+ function getPlayerName(room, side) {
+   var game = games[room];
+   for (var p in game.players) {
+     var player = game.players[p];
+     if (player.side === side) {
+       return player.name;
+     }
+   }
+ }
 
-}
+};
