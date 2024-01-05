bc969209058518074a237ffc425141e8292897dc
kantamat <kantamat@amazon.co.jp>
2024-01-05 21:18:35 +0900

    01/05 1837

--- routes/search.js
+++ routes/search.js
@@ -3,20 +3,30 @@ const { defaultProvider } = require("@aws-sdk/credential-provider-node"); // V3 
 const { Client } = require('@opensearch-project/opensearch');
 const { AwsSigv4Signer } = require('@opensearch-project/opensearch/aws');
 
+
+
 const client = new Client({
     ...AwsSigv4Signer({
-      region: process.env.AWS_REGION || 'us-east-1',  
-      service: process.env.SERVICE || 'es',
+      region: 'us-east-1',
+      service: 'es',  // 'aoss' for OpenSearch Serverless
+      // Must return a Promise that resolve to an AWS.Credentials object.
+      // This function is used to acquire the credentials when the client start and
+      // when the credentials are expired.
+      // The Client will refresh the Credentials only when they are expired.
+      // With AWS SDK V2, Credentials.refreshPromise is used when available to refresh the credentials.
+  
+      // Example with AWS SDK V3:
       getCredentials: () => {
+        // Any other method to acquire a new Credentials object can be used.
         const credentialsProvider = defaultProvider();
         return credentialsProvider();
       },
     }),
-    nodes: ['https://search-game-ws-q7zmcbu77egwwwobsncuukjwuq.us-east-1.es.amazonaws.com'], // OpenSearchエンドポイント
-  auth: {
-    username: 'opensearch',
-    password: 'Opensearch0!'
-  }
+    node: 'https://search-game-ws-q7zmcbu77egwwwobsncuukjwuq.us-east-1.es.amazonaws.com', // OpenSearchエンドポイント
+    auth: {
+        username: 'opensearch',
+        password: 'Opensearch0!'
+    }
 });
 
 var router = express.Router();
