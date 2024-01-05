3a9750568d64e9cc9200979b6285f47293d7974c
kantamat <kantamat@amazon.co.jp>
2024-01-05 20:54:25 +0900

    01/05 1837

--- routes/search.js
+++ routes/search.js
@@ -1,53 +1,61 @@
 var express = require('express');
+const { defaultProvider } = require("@aws-sdk/credential-provider-node"); // V3 SDK.
+const { Client } = require('@opensearch-project/opensearch');
+const { AwsSigv4Signer } = require('@opensearch-project/opensearch/aws');
 
-var opensearch = require('@opensearch-project/opensearch');
-
-// OpenSearchに接続する設定
-var client = new opensearch.Client({
-    nodes: [
-        'https://search-game-ws-q7zmcbu77egwwwobsncuukjwuq.us-east-1.es.amazonaws.com'
-      ],
-    auth: {
-        username: 'opensearch',
-        password: 'Opensearch0!'
-    },
-    log: 'trace'
+const client = new Client({
+    ...AwsSigv4Signer({
+      region: process.env.AWS_REGION || 'us-east-1',  
+      service: process.env.SERVICE || 'es',
+      getCredentials: () => {
+        const credentialsProvider = defaultProvider();
+        return credentialsProvider();
+      },
+    }),
+  node: 'https://search-game-ws-q7zmcbu77egwwwobsncuukjwuq.us-east-1.es.amazonaws.com', // OpenSearchエンドポイント
+  auth: {
+    username: 'opensearch',
+    password: 'Opensearch0!'
+  }
 });
 
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('partials/search', {
-        title: 'Chess Hub - Search',
-        user: req.user,
-        isSearchPage: true
-    });
+  res.render('partials/search', {
+    title: 'Chess Hub - Search', 
+    user: req.user,
+    isSearchPage: true
+  });
 });
 
-router.post('/', function (req, res) {
+router.post('/', async function(req, res) { // async/awaitを使用
 
-    var body = {
-        query: {
-            term: {
-                white: req.body.white
-            }
-        }
-    };
-
-    client.search({
-        index: 'chesshub',
-        body: body
-    }).then(function (resp) {
-        var games = resp.hits.hits;
-
-        res.set('Content-Type', 'application/json');
-        res.status(200);
-        res.send({ games: games });
-    }).catch(function (err) {
-        res.status(500);
-        console.log(err);
+  var body = {
+    query: {
+      term: {
+        white: req.body.white
+      }
+    }
+  };
+  
+  try {
+    var results = await client.search({ // awaitを使用して非同期処理を待機
+      index: 'chesshub',
+      body: body
     });
+    
+    var games = results.hits.hits;
 
+    res.set('Content-Type', 'application/json');
+    res.status(200);
+    res.send({games: games});
+    
+  } catch(err) {
+    res.status(500);
+    console.log(err);
+  }
+  
 });
 
 module.exports = router;
