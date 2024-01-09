742bb8d545e2f18ae34cc46465d3f1ce41bfd0bc
kantamat <kantamat@amazon.co.jp>
2024-01-09 15:48:11 +0900

    0109 1548

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// app.js
+
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(url);
+
 var fs = require('fs');
 var express = require('express');
-var http = require('http');
+var http = require('http'); 
 var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -12,10 +17,7 @@ var flash = require('connect-flash');
 const mongoose = require('mongoose');
 
 // Expressアプリケーション作成
-const app = express(); 
-
-// MongoDB接続設定をインポート
-const db = require('./config/database.js');
+const app = express();
 
 // configure express app
 app.set('views', path.join(__dirname, 'views'));
@@ -26,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser('S3CRE7'));
 app.use(flash());
-app.use(session({ secret: 'S3CRE7-S3SSI0N', saveUninitialized: true, resave: true } ));
+app.use(session({ secret: 'S3CRE7-S3SSI0N', saveUninitialized: true, resave: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 require('./config/passport')(app, passport);
 app.use(passport.initialize());
@@ -34,14 +36,14 @@ app.use(passport.session());
 
 // configure routes
 var routes = require('./routes/index');
-var account = require('./routes/account');
+var account = require('./routes/account'); 
 var api = require('./routes/api');
 var play = require('./routes/play');
 var login = require('./routes/login');
 var register = require('./routes/register');
 var search = require('./routes/search');
 
-app.use('/', routes);
+app.use('/', routes);  
 app.use('/login', login);
 app.use('/register', register);
 app.use('/account', account);
@@ -49,25 +51,33 @@ app.use('/play', play);
 app.use('/api', api);
 app.use('/search', search);
 
-// configure error handlers
+// configure error handlers  
 require('./config/errorHandlers.js')(app);
 
 // Promiseベースの非同期処理
-app.get('/', (req, res) => {
-    Game.find({})
-      .then(games => {
-        res.render('index', {games});  
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(500).send('Error fetching games');
-      });
-  });
+app.get('/', async (req, res) => {
+  const games = await getGames(client);
+  res.render('index', {games});
+});
+
+async function getGames(client) {
+  // ...
+}
+
+async function start() {
+  await client.connect();
+
+  // アプリケーション起動
+
+  await client.close();
+}
+
+start();
 
 // エラーハンドリング
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error'); 
-  });
+  console.error(err);
+  res.status(500).send('Internal Server Error');
+});
 
 module.exports = app;
