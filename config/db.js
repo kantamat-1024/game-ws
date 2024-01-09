0af18242e6e0cb7288b8c5b9461c3be82167571d
kantamat <kantamat@amazon.co.jp>
2024-01-09 10:20:37 +0900

    01/09 1016

--- config/db.js
+++ config/db.js
@@ -1,5 +1,18 @@
+const config = require('config');
+const mongoose = require('mongoose');
+
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/chesshub';
 
 module.exports = {
   url: mongoUrl
 };
+
+const options = {
+  poolSize: config.get('chesshub.poolSize'),
+  socketTimeoutMS: config.get('chesshub.socketTimeoutMS'),
+  connectTimeoutMS: config.get('chesshub.connectTimeoutMS'),
+  reconnectTries: config.get('chesshub.reconnectTries'), 
+  reconnectInterval: config.get('chesshub.reconnectInterval')
+};
+
+mongoose.connect(config.get('chesshub.db'), options);
--- config/mongodb.js
+++ config/mongodb.js
@@ -1,19 +1,30 @@
-// MongoDB接続設定
+// 接続設定
 const mongoose = require('mongoose');
 const config = require('config');
 
 // コネクションプールの設定
 const options = {
-  poolSize: 10, 
-  socketTimeoutMS: 30000,
-  connectTimeoutMS: 30000
+  poolSize: 50,
+  socketTimeoutMS: 30000, 
+  connectTimeoutMS: 30000,
+  reconnectTries: 30, // リトライ回数
+  reconnectInterval: 1000 // リトライ間隔(ミリ秒)
 };
 
-mongoose.connect(config.mongodb.uri, options);
+// 接続処理
+mongoose.connect(config.mongodb.uri, options); 
+
+// リトライロジック
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected. Retrying connection...');
+  setTimeout(() => {
+    mongoose.connect(config.mongodb.uri, options);
+  }, options.reconnectInterval);
+});
 
 // エラーハンドリング
 mongoose.connection.on('error', err => {
   console.error(err);
 });
 
-module.exports = mongoose.connection; 
+module.exports = mongoose.connection;
